3b2b17b0cd5c6d1e6b3349217372e93efe563a8b
Violetta Bayeva <violetta.bayeva@dreamstormstudios.com>
2022-09-07 19:51:19 +0200

    Change index to use react

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  return <>Hello World</>;
+  return <div>Hello! There will be text and some awesome stuff soon, I promise.</div>;
 }
 
 export default App;
